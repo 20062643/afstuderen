dbdf9f140e84011882ef1d63c2a581a8bf50d227
Michiel Sikkes <michiel.sikkes@gmail.com>
2013-09-29 18:51:28 +0200

    Update prototype code

--- protopolar/cipher.c
+++ protopolar/cipher.c
@@ -5,25 +5,29 @@
 int main()
 {
   cipher_context_t ctx;
-  const char key[16] = "test";
-  const char input[20] = "halloinputballoha";
-  unsigned char output[20];
+  const char key[16] = "1234567890123456";
+  const char *input = "Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Sed posuere consectetur est at lobortis. Curabitur blandit tempus porttitor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed posuere consectetur est at lobortis.";
+  char output[1024];
+  int ret;
   memset(output, 0, sizeof(output));
 
-  size_t olen = 20;
+  size_t olen = 0;
 
   cipher_init_ctx(&ctx, cipher_info_from_type(POLARSSL_CIPHER_AES_128_CTR));
-  cipher_setkey(&ctx, (const unsigned char *) key, 128, POLARSSL_ENCRYPT);
+  ret = cipher_setkey(&ctx, (const unsigned char *) key, 128, POLARSSL_ENCRYPT);
+  printf("%d\n", ret);
   cipher_update(&ctx, (const unsigned char *) input, strlen(input), output, &olen);
-  cipher_finish(&ctx, output, &olen);
 
-  printf("%s\n", output);
+  printf("%zu\n", olen);
+  printf("%zu\n", strlen(output) );
+
+  cipher_finish(&ctx, output, &olen);
 
   unsigned char dst[1024];
 
   size_t dlen = 1024;
 
-  base64_encode(dst, &dlen, output, strlen(input));
+  base64_encode(dst, &dlen, output, strlen(input) );
 
   printf("%s\n", dst);
 
--- protopolar/encrypt_ctr.c
+++ protopolar/encrypt_ctr.c
@@ -6,39 +6,36 @@
 
 int main()
 {
-  unsigned char key[16] = "polarsslaaab";
-  const char input[] = "{batsmy_json: 'yes', mit: 'awesomde', cool: 'very cool', wow: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula porta felis euismod semper. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.}";
+  unsigned char key[16] = "123456789012345";
+  const char input[] = "testhalloditiseenlanfdsafdsafdsafdsafdasfadsgetesthalloditiseenlanfdsafdsafdsafdsafdasfadsgetesthalloditiseenlanfdsafdsafdsafdsafdasfadsgetesthalloditiseenlanfdsafdsafdsafdsafdasfadsgetesthalloditiseenlanfdsafdsafdsafdsafdasfadsgetesthalloditiseenlanfdsafdsafdsafdsafdasfadsgetesthalloditiseenlanfdsafdsafdsafdsafdasfadsgetesthalloditiseenlanfdsafdsafdsafdsafdasfadsge";
 
   printf("%s\n", key);
-  printf("%zu\n", sizeof(key));
-  printf("%zu\n", strlen(key));
 
-  unsigned char output[strlen(input)];
+  unsigned char output[sizeof(input)];
 
   aes_context ctx;
 
-  size_t length = strlen(input);
   size_t nc_off = 0;
 
   unsigned char nonce_counter[16];
   unsigned char stream_block[16];
 
-  memset(nonce_counter, 0, sizeof(nonce_counter));
-  memset(stream_block, 0, sizeof(stream_block));
-  memset(output, 0, sizeof(output));
+  // memset(nonce_counter, 0, sizeof(nonce_counter));
+  // memset(stream_block, 0, sizeof(stream_block));
+  // memset(output, 0, sizeof(output));
 
   aes_setkey_enc(&ctx, key, 128);
 
-  aes_crypt_ctr(&ctx, length, &nc_off, nonce_counter, stream_block, (unsigned char *) input, output);
+  aes_crypt_ctr(&ctx, sizeof(input), &nc_off, nonce_counter, stream_block, (unsigned char *) input, output);
 
   // printf("%zu\n", nc_off);
   // printf("%zu\n", strlen(input));
 
-  unsigned char dst[(int) ceil(length * 1.4)];
+  unsigned char dst[1024];
 
-  size_t dlen = (int) ceil(length * 1.4);
+  size_t dlen = 1024;
 
-  base64_encode(dst, &dlen, output, sizeof(output));
+  base64_encode(dst, &dlen, output, sizeof(input));
 
   printf("%s\n%zu\n", dst, strlen((char *)dst));
 
